feb934d7fdf63e8dbb3e4bb11ac62d724ebe2306
maelstromm <maelstromm@maelstromms-MacBook-Pro.local>
2025-02-12 22:38:21 +0300

    add services list

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,15 +1,28 @@
-import { UnlockMarketAccounts } from '$components/Services/UnlockMarketAccounts/UnlockMarketAccounts';
-import { UnlockBankAccounts } from '$components/Services/UnlockBankAccounts/UnlockBankAccounts';
+import React, { FC } from "react";
+// import { UnlockMarketAccounts } from '$components/Services/UnlockMarketAccounts/UnlockMarketAccounts';
+// import { UnlockBankAccounts } from '$components/Services/UnlockBankAccounts/UnlockBankAccounts';
+import { ServiceListComponent } from "$components/Services/serviceListComponent";
+import { cryptoServiceList, individualsServiceList, legalEntitiesServiceList } from "$components/Services/serviceList";
 
-export const UnlockServices = () => {
-    return (
-        <>
-            <div className="text-2xl md:text-6xl mb-16">
-                <span>Мы можем помочь:</span>
-            </div>
+// export const UnlockServices = () => {
+//     return (
+//         <>
+//             <div className="text-2xl md:text-6xl mb-16">
+//                 <span>Мы можем помочь:</span>
+//             </div>
+//
+//             <UnlockMarketAccounts />
+//             <UnlockBankAccounts />
+//         </>
+//     )
+// }
 
-            <UnlockMarketAccounts />
-            <UnlockBankAccounts />
-        </>
-    )
+export const UnlockServices: FC = () => {
+  return (
+    <div className='flex flex-col gap-12'>
+      <ServiceListComponent title='Физическим лицам:' list={individualsServiceList} />
+      <ServiceListComponent title='Юридическим лицам:' list={legalEntitiesServiceList} />
+      <ServiceListComponent title='Криптовалютный сектор:' list={cryptoServiceList} />
+    </div>
+  )
 }
