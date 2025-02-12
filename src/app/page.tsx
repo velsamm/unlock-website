d52bc3afb8dc646e62e212e94835931d2af3eef6
maelstromm <maelstromm@maelstromms-MacBook-Pro.local>
2025-02-12 23:53:55 +0300

    small redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,34 @@
 import Image from "next/image";
 import { WhoWorks } from "$components/WhoWorks/WhoWorks";
 import { HelpWith } from "$components/HelpWith/HelpWith";
-// import { UnlockServices } from "$components/Services";
 import { HowWeWork } from "$components/HowWeWork/HowWeWork";
-import { Price } from "$components/Price";
-// import { TG } from "$components/TG/TG";
 import { ContactForm } from "$components/ContactForm/ContactForm";
 import manOnPcPic from '$assets/icons/svg/ManOnPC.svg'
 
 export default function Home() {
   return (
     <main>
-      <div className="mt-8 md:mt-32">
-        <h1 className="text-2xl md:text-6xl">
-          Разблокируем доступ к средствам на криптовалютных биржах
-          и банковских счетах
-        </h1>
-      </div>
-      <div className="flex justify-between flex-col-reverse md:flex-row">
-        <ContactForm/>
-        <div className="image-man-on-pc">
-          <Image src={manOnPcPic} alt='manOnPcPic'/>
+      <section className="pt-0 md:pt-32 cta flex flex-col justify-around">
+        <div>
+          <h1 className="text-2xl md:text-6xl">
+            Разблокируем доступ к средствам на криптовалютных биржах
+            и банковских счетах
+          </h1>
         </div>
-      </div>
+        <div className="flex justify-between flex-col-reverse md:flex-row items-center">
+          <ContactForm/>
+          <div className="image-man-on-pc">
+            <Image src={manOnPcPic} alt='manOnPcPic'/>
+          </div>
+        </div>
+      </section>
 
-      <div className="mt-20" />
+      <div className="mt-20"/>
       <WhoWorks/>
       <HelpWith/>
 
-      <div className="mt-20" />
-
-      {/*<UnlockServices/>*/}
+      <div className="mt-20"/>
       <HowWeWork/>
-
-      <div id='prices' />
-      <Price/>
-
-      {/*<TG/>*/}
     </main>
   )
 }
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export const ContactForm = () => {
   return (
     <section className='my-auto mt-8 md:m-0'>
-      <p className='text-2xl md:text-5xl'>
+      <h2 className='text-2xl md:text-5xl'>
         Получить консультацию
-      </p>
+      </h2>
       <button className="btn my-5">В телеграме</button>
       <button className="btn">По эл. почте</button>
     </section>
