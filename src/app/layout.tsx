646e950e30a5fac6817452c27eeaa7407f5cf78d
maelstromm <email@email.cpm>
2025-04-22 22:01:46 +0300

    background particles effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "$components/Header/Header";
 import { Footer } from "$components/Footer/Footer";
+import { ParticlesContainer } from '$components/particles/particles';
 
 export const metadata: Metadata = {
   title: "Разблокировка аккаунтов",
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme='dark'>
     <body className='app'>
+      <ParticlesContainer />
       <Header/>
       <section className='flex flex-1 flex-col'>
         {children}
---
+++ src/components/particles/particles.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { IParticlesProps } from "@tsparticles/react/dist/IParticlesProps";
+
+export function ParticlesContainer(props: unknown) {
+	const [ init, setInit ] = useState(false);
+
+	// this should be run only once per application lifetime
+	useEffect(() => {
+		initParticlesEngine(async (engine) => {
+			// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+			// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+			// starting from v2 you can add only the features you need reducing the bundle size
+			//await loadAll(engine);
+			//await loadFull(engine);
+			await loadSlim(engine);
+			//await loadBasic(engine);
+		}).then(() => {
+			setInit(true);
+		});
+	}, []);
+
+	const particlesLoaded: IParticlesProps['particlesLoaded'] = async (container) => {
+		console.log(container);
+	};
+
+	const options: IParticlesProps['options'] = {
+		fullScreen: {
+			zIndex: -1,
+		},
+		background: {
+			color: {
+				value: "transparent",
+			},
+		},
+		fpsLimit: 120,
+		interactivity: {
+			events: {
+				onClick: {
+					enable: false,
+					mode: "push",
+				},
+				onHover: {
+					enable: false,
+					mode: "repulse",
+				},
+				resize: true,
+			},
+			modes: {
+				push: {
+					quantity: 4,
+				},
+				repulse: {
+					distance: 200,
+					duration: 0.4,
+				},
+			},
+		},
+		particles: {
+			color: {
+				value: "#ffffff",
+			},
+			links: {
+				color: "#ffffff",
+				distance: 150,
+				enable: true,
+				opacity: 0.5,
+				width: 1,
+			},
+			move: {
+				direction: "none",
+				enable: true,
+				outModes: {
+					default: "bounce",
+				},
+				random: false,
+				speed: 2,
+				straight: false,
+			},
+			number: {
+				density: {
+					enable: true,
+					area: 800,
+				},
+				value: 120,
+			},
+			opacity: {
+				value: 0.15,
+			},
+			shape: {
+				type: "circle",
+			},
+			size: {
+				value: { min: 1, max: 5 },
+			},
+		},
+		detectRetina: true,
+	};
+
+	if (!init) {
+		return null;
+	}
+
+	return <Particles particlesLoaded={particlesLoaded} options={options} />;
+}
