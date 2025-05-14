5f2b6ef3b23587cdcab2faaaea23256e759fb9fd
maelstromm <email@email.cpm>
2025-05-14 19:38:28 +0300

    add more blog entries. make blog readable

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -7,10 +7,12 @@ import Link from "next/link";
 
 export const Blog: FC = () => {
 	return (
-		<article className='flex gap-10 flex-wrap justify-items-start mt-20'>
+		<article className='flex gap-10 flex-wrap justify-items-start mt-10 rounded-2xl bg-[--background-color] p-10 shadow-2xl shadow-[--background-color]/30'>
+			<Typography as='p' className='mb-10 w-full' appearance='header'>Блог</Typography>
+
 			{posts.map((post) => (
 				<Link key={post.title} href={'/blog/' + post.id}>
-					<article className='max-w-72'>
+					<article className='max-w-32 md:max-w-52'>
 						<ImageWrapper src={noImage}  className='rounded-lg'/>
 						<Typography as='p' className='my-2 text-gray-400'>{new Date(post.date).toLocaleString('ru')}</Typography>
 						<Typography as='h1' appearance='regular' >
--- src/components/blog/blogEntry.tsx
+++ src/components/blog/blogEntry.tsx
@@ -10,17 +10,17 @@ export const BlogEntry: FC<{ id: string }> = ({ id }) => {
   }
 
   return (
-    <article>
-      <Typography as='p' className='my-2 text-gray-400'>{new Date(post.date).toLocaleString('ru')}</Typography>
-
-      <Typography as='h1' appearance='header' className='mt-20 mb-20' >
+    <article className='mt-10 rounded-2xl bg-[--background-color] px-10 shadow-2xl shadow-[--background-color]/30'>
+      <Typography as='h1' appearance='header' className='my-10' >
         {post.title}
       </Typography>
+
+      <Typography as='p' className='mb-10 text-gray-400'>{new Date(post.date).toLocaleString('ru')}</Typography>
 
       {post.blocks.map((block, index) => (
         <Typography
           key={index}
-          className="my-6"
+          className="my-6 tracking-wide"
           as="p"
           appearance="regular"
           dangerouslySetInnerHTML={{ __html: block.data.text || '' }}
