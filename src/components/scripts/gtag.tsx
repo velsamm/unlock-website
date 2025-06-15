b7a988c746af37f2aa4266f5dfb2a19688b5184b
maelstromm <email@email.cpm>
2025-06-15 21:58:10 +0300

    fix ga script

--- src/components/scripts/gtag.tsx
+++ src/components/scripts/gtag.tsx
@@ -3,18 +3,18 @@
 import Script from "next/script";
 
 const gtag = `
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-T3VCXBS63Z"></script>
-<script>
-	window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || [];
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
 
 	gtag('config', 'G-T3VCXBS63Z');
-</script>
 `;
 
 export const GTAGScript = () => {
-
-	return <Script id='gtag-script' dangerouslySetInnerHTML={{ __html: gtag }} strategy='beforeInteractive' />
+	return (
+		<>
+			<Script id='gtag-init' dangerouslySetInnerHTML={{ __html: gtag }} />
+			<Script id='gtag' src='https://www.googletagmanager.com/gtag/js?id=G-T3VCXBS63Z' />
+		</>
+	)
 }
